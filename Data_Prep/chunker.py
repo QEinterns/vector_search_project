fc5742186511046f96abe7b97de02a3b5e8def02
Sanjivani Patra <sanjivani.patra@couchbase.com>
2024-02-28 10:34:05 +0530

    Implemented deduplication mechanism

--- Data_Prep/chunker.py
+++ Data_Prep/chunker.py
@@ -1,4 +1,13 @@
 from dependencies import *
+import hashlib
+
+# Global set to store unique chunk hash values across all files
+global_unique_hashes = set()
+
+def hash_text(text):
+    # Generate a hash value for the text using SHA-256
+    hash_object = hashlib.sha256(text.encode())
+    return hash_object.hexdigest()
 
 def chunk_text(text, title, Chunk_size = 2000, Overlap = 50, Length_function = len, debug_mode = 0):
     global global_unique_hashes
@@ -15,9 +24,17 @@ def chunk_text(text, title, Chunk_size = 2000, Overlap = 50, Length_function = l
             print(f"Chunk {idx+1}: {chunk}\n")
         
         print('\n')
+    
+    # Deduplication mechanism
+    unique_chunks = []
+    for chunk in chunks:
+        chunk_hash = hash_text(chunk.page_content)
+        if chunk_hash not in global_unique_hashes:
+            unique_chunks.append(chunk)
+            global_unique_hashes.add(chunk_hash)
 
-    for sentence in chunks:
+    for sentence in unique_chunks:
         sentence.page_content = title + " " + sentence.page_content
 
-    # print(chunks)
-    return chunks
+    print(unique_chunks)
+    return unique_chunks
