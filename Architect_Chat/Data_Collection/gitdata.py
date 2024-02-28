09a42c19f83530e06da6a1f1f65210f5ca7be6bd
Sanjivani Patra <sanjivani.patra@couchbase.com>
2024-02-28 12:19:58 +0530

    Code to download docs from Github repos

--- Architect_Chat/Data_Collection/gitdata.py
+++ Architect_Chat/Data_Collection/gitdata.py
@@ -1,9 +1,23 @@
 import config
 import requests
+import os
 
-def get_repository_files_recursively(owner, repo, path='', branch='master', token=None):
+def download_file(url, file_path):
     """
-    Retrieve all file names recursively in a directory of a repository from GitHub.
+    Download a file from the specified URL to the specified file path.
+
+    Args:
+    - url (str): URL of the file to download.
+    - file_path (str): Local file path where the downloaded file will be saved.
+    """
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an exception for HTTP errors
+    with open(file_path, 'wb') as f:
+        f.write(response.content)
+
+def download_repository_files(owner, repo, path='', branch='master', token=None, download_dir='downloaded_files'):
+    """
+    Download all PDF files in a directory of a repository from GitHub, including subdirectories.
 
     Args:
     - owner (str): Username or organization name which owns the repository.
@@ -11,9 +25,10 @@ def get_repository_files_recursively(owner, repo, path='', branch='master', toke
     - path (str, optional): Path within the repository. Default is root directory.
     - branch (str, optional): Branch name. Default is 'master'.
     - token (str, optional): GitHub personal access token for authentication.
+    - download_dir (str, optional): Local directory where the downloaded files will be saved.
 
     Returns:
-    - list: List of all file names in the specified directory and its subdirectories.
+    - list: List of PDF file names downloaded.
     """
     headers = {}
     if token:
@@ -22,13 +37,16 @@ def get_repository_files_recursively(owner, repo, path='', branch='master', toke
     response = requests.get(url, headers=headers)
     response.raise_for_status()  # Raise an exception for HTTP errors
     contents = response.json()
-    files = []
+    downloaded_files = []
     for content in contents:
-        if content['type'] == 'file':
-            files.append(content['name'])
+        if content['type'] == 'file' and content['name'].endswith('.pdf'):
+            file_name = os.path.join(download_dir, content['name'])
+            download_url = content['download_url']
+            download_file(download_url, file_name)
+            downloaded_files.append(file_name)
         elif content['type'] == 'dir':
-            files.extend(get_repository_files_recursively(owner, repo, content['path'], branch, token))
-    return files
+            downloaded_files.extend(download_repository_files(owner, repo, content['path'], branch, token, download_dir))
+    return downloaded_files
 
 # Example usage
 owner = 'couchbase'
@@ -36,6 +54,6 @@ repo = 'magma'
 path = ''  # Optional: specify the directory path within the repository
 branch = 'master'  # Specify the branch name
 token = config.github_passkey
-file_names = get_repository_files_recursively(owner, repo, path, branch, token)
-for i in file_names:
-    print(i)
+download_dir = '/Users/spatra/Desktop/Movie/Architect_Chat/Data_Collection/data'  # Specify the directory where files will be downloaded
+downloaded_files = download_repository_files(owner, repo, path, branch, token, download_dir)
+print("Downloaded files:", downloaded_files)
