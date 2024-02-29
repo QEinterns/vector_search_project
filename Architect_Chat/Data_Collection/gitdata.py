df7d74f1e7efacb0321046394140d8fc6a5392b5
Sanjivani Patra <sanjivani.patra@couchbase.com>
2024-02-29 10:54:21 +0530

    Added .txt files to .gitignore

--- Architect_Chat/Data_Collection/gitdata.py
+++ Architect_Chat/Data_Collection/gitdata.py
@@ -2,6 +2,35 @@ import config
 import requests
 import os
 
+# def get_repository_files_recursively(owner, repo, path='', branch='master', token=None):
+#     """
+#     Retrieve all file names recursively in a directory of a repository from GitHub.
+
+#     Args:
+#     - owner (str): Username or organization name which owns the repository.
+#     - repo (str): Name of the repository.
+#     - path (str, optional): Path within the repository. Default is root directory.
+#     - branch (str, optional): Branch name. Default is 'master'.
+#     - token (str, optional): GitHub personal access token for authentication.
+
+#     Returns:
+#     - list: List of all file names in the specified directory and its subdirectories.
+#     """
+#     headers = {}
+#     if token:
+#         headers['Authorization'] = f'token {token}'
+#     url = f'https://api.github.com/repos/{owner}/{repo}/contents/{path}?ref={branch}'
+#     response = requests.get(url, headers=headers)
+#     response.raise_for_status()  # Raise an exception for HTTP errors
+#     contents = response.json()
+#     files = []
+#     for content in contents:
+#         if content['type'] == 'file':
+#             files.append(content['name'])
+#         elif content['type'] == 'dir':
+#             files.extend(get_repository_files_recursively(owner, repo, content['path'], branch, token))
+#     return files
+
 def download_file(url, file_path):
     """
     Download a file from the specified URL to the specified file path.
