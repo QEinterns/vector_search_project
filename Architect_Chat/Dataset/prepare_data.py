ae8886d4fd4cef30c4ae7d9f155ee08adfd3a16c
Sanjivani Patra <sanjivani.patra@couchbase.com>
2024-03-03 20:45:47 +0530

    Code to extract content from pdfs

--- Architect_Chat/Dataset/prepare_data.py
+++ Architect_Chat/Dataset/prepare_data.py
@@ -6,17 +6,19 @@ def extract_text_from_pdf(pdf_file):
     reader = PdfReader(pdf_file)
     text = ''
     page = reader.pages[0]
-    print(page.extract_text())
-    text += multiline_to_single_conv(page.extract_text())
+    # print(page.extract_text())
+    for page in reader.pages:
+        text += multiline_to_single_conv(page.extract_text())
     return text
 
 def main():
-    directory = '/Users/spatra/Desktop/Movie/Architect_Chat/Data_Collection/data'
-    output_file = '/Users/spatra/Desktop/Movie/Architect_Chat/Dataset/raw_data.txt'
+    directory = '/Users/spatra/Desktop/Movie/Internal_docs/xdcr'
+    output_file = '/Users/spatra/Desktop/Movie/Architect_Chat/Dataset/raw/xdcr.txt'
 
-    with open(output_file, 'w', encoding='utf-8') as output:
+    with open(output_file, 'a', encoding='utf-8') as output:
         for filename in os.listdir(directory):
             if filename.endswith('.pdf'):
+                print(filename)
                 pdf_path = os.path.join(directory, filename)
                 text = extract_text_from_pdf(pdf_path)
                 output.write(text + ' ')  # Add a couple of newlines between texts for clarity
